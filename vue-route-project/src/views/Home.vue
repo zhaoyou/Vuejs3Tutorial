748d6d4652113e1f8dfe9c5efaf9ed433c7c799d
zhaoyou <zhaoyou.xt@gmail.com>
2023-12-11 20:04:20 +0800

    test

--- vue-route-project/src/views/Home.vue
+++ vue-route-project/src/views/Home.vue
@@ -3,8 +3,25 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
 export default {
+  setup(props) {
+    console.log(props)
 
+    onMounted(
+      () => {
+        console.log('onMounted')
+      }
+    )
+
+    onUpdated(() => {
+      console.log('onUpdated')
+    })
+
+    onUnmounted(() => {
+      console.log('onUmmounted')
+    })
+  }
 }
 </script>
 
